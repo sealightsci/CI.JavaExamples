fe47c60897182e7edd197ba476c76d08ed5a79d2
alasch2 <ala.schneider@sealights.io>
2018-05-29 11:24:57 +0300

    example-junit-testng - added testng+powermock tests

---
+++ example-junit-testng/src/main/java/io/sl/ex/junittestng/calculator/CalculatorUse.java
@@ -0,0 +1,22 @@
+package io.sl.ex.junittestng.calculator;
+
+public class CalculatorUse {
+	
+	private Calculator calc;
+	
+	public CalculatorUse() {
+		calc = CalculatorFactory.createCalculator();
+	}
+
+	public int calcSum(int a, int b) {
+		return calc.doSum(a, b);
+	}
+
+	public int calcSub(int a, int b) {
+		return calc.doSub(a, b);
+	}
+	
+	public int calcMul(int a, int b) {
+		return calc.doMul(a, b);
+	}
+}
